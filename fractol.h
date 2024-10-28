fdc0249acf708c13cac05382c47c366d307f6a49
Sara García Miguel <146083097+saragar2@users.noreply.github.com>
2024-10-28 11:56:51 +0100

    organized and comments added
    -end checking norminette

--- fractol.h
+++ fractol.h
@@ -65,31 +65,42 @@ typedef struct s_Image
 	int		multip;
 }	t_Image;
 
+// -----------parsing
+void	init_img_soft(t_Image *i, char **argv);
+void	init_img(t_Image *img);
+void	select_fractal(t_Image *img);
+int		go_exit_cross(t_Image *param);
+int		key_control(int keycode, t_Image *param);
+void	change_color(int keycode, t_Image *img);
+int		on_destroy(t_Image *i);
+
+// -----------base fractal func
+void	generate_julia_set(t_Image img);
+void	generate_mandel_set(t_Image img);
+void	generate_bs_set(t_Image img);
+
+// -----------maths
 double	complex_magnitude_squared(t_Complex z);
 int		set_iteration(t_Complex z, t_Complex c, int maxIterations);
-void	generate_julia_set(t_Image img);
+int		bs_set_iteration(t_Complex z, t_Complex c, int maxIterations);
+
+// -----------movement / zoom / image
+void	put_pixel_in_img(t_Image *img, int x, int y, int color);
+int		new_zoom(int button, int x, int y, t_Image *img);
+int		move_left_right(int keycode, t_Image *img);
+void	apply_zoom(t_Image *img, t_Mandm *g);
+
+// -----------color
 int		select_color(int iterations, int max_iterations, t_Image img);
 void	first_aux(int *iterations, int *red, int *green, int *blue);
 void	second_aux(int *iterations, int *red, int *green, int *blue);
+void	color_fix(int *iterations, int *blue);
 int		rgb_to_hex(int red, int green, int blue);
-int		key_control(int keycode, t_Image *param);
-int		go_exit_cross(t_Image *param);
-void	init_img(t_Image *img);
-void	init_img_soft(t_Image *i, char **argv);
-void	put_pixel_in_img(t_Image *img, int x, int y, int color);
-int		new_zoom(int button, int x, int y, t_Image *img);
-void	generate_mandel_set(t_Image img);
+
+// -----------utils
 void	f_printf(char const *str);
 int		f_strcmp(const char *s1, const char *s2);
-int		move_left_right(int keycode, t_Image *img);
-void	invert_color(t_Image *img);
-void	apply_zoom(t_Image *img, t_Mandm *g);
-void	change_color(int keycode, t_Image *img);
-void	color_fix(int *iterations, int *blue);
-void	generate_bs_set(t_Image img);
-int		bs_set_iteration(t_Complex z, t_Complex c, int maxIterations);
-void	select_fractal(t_Image *img);
-int		on_destroy(t_Image *i);
+
 
 # ifndef WIDTH
 #  define WIDTH 250
